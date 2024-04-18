788df34ae15f9af7c587ff45203408cced9bdd9c
Muhammad Musawir <139618320+Muhammad-Musawir@users.noreply.github.com>
2024-04-18 12:30:56 +0800

    Update Employee.h with adding the necessary fields to the Employee

--- Coding Assingment/Employee.h
+++ Coding Assingment/Employee.h
@@ -3,41 +3,49 @@
 #include <string>
 
 namespace Records {
-	const int kDefaultStartingSalary = 30000;
+    const int kDefaultStartingSalary = 30000;
 
-	class Employee
-	{
-	public:
-		Employee() = default;
-		Employee(const std::string& firstName, const std::string& lastName);
+    class Employee {
+    public:
+        Employee() = default;
+        Employee(const std::string& firstName, const std::string& middleName, const std::string& lastName);
 
-		void promote(int raiseAmount = 1000);
-		void demote(int demeritAmount = 1000);
-		void hire(); // Hires or rehires the employee
-		void fire(); // Dismisses the employee
-		void display() const;// Outputs employee info to console
+        void promote(int raiseAmount = 1000);
+        void demote(int demeritAmount = 1000);
+        void hire(); // Hires or rehires the employee
+        void fire(); // Dismisses the employee
+        void display() const; // Outputs employee info to console
 
-		// Getters and setters
-		void setFirstName(const std::string& firstName);
-		const std::string& getFirstName() const;
+        // Getters and setters
+        void setFirstName(const std::string& firstName);
+        const std::string& getFirstName() const;
 
-		void setLastName(const std::string& lastName);
-		const std::string& getLastName() const;
+        void setMiddleName(const std::string& middleName);
+        const std::string& getMiddleName() const;
 
-		void setEmployeeNumber(int employeeNumber);
-		int getEmployeeNumber() const;
+        void setLastName(const std::string& lastName);
+        const std::string& getLastName() const;
 
-		void setSalary(int newSalary);
-		int getSalary() const;
+        void setAddress(const std::string& address);
+        const std::string& getAddress() const;
 
-		bool isHired() const;
+        void setEmployeeNumber(int employeeNumber);
+        int getEmployeeNumber() const;
 
-	private:
-		std::string mFirstName;
-		std::string mLastName;
-		int mEmployeeNumber = -1;
-		int mSalary = kDefaultStartingSalary;
-		bool mHired = false;
-	};
+        void setSalary(int newSalary);
+        int getSalary() const;
+
+        bool isHired() const;
+
+    private:
+        std::string mFirstName;
+        std::string mMiddleName;
+        std::string mLastName;
+        std::string mAddress;
+        int mEmployeeNumber = -1;
+        int mSalary = kDefaultStartingSalary;
+        bool mHired = false;
+    };
 }
+
 
