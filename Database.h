1536d5fe40f20e31e1cdf9b894e308f518c87a9b
Muhammad Musawir <139618320+Muhammad-Musawir@users.noreply.github.com>
2024-04-19 12:26:04 +0800

    Implemented the editEmployee function

--- Database.h
+++ Database.h
@@ -5,29 +5,27 @@
 #include "Employee.h"
 
 namespace Records {
-	const int kFirstEmployeeNumber = 1000;
+    const int kFirstEmployeeNumber = 1000;
 
-	class Database
-	{
-	public:
-		Employee& addEmployee(const std::string& firstName,
-		                      const std::string& middleName,
-							  const std::string& lastName,
-							  const std::string& address);
-		Employee& getEmployee(int employeeNumber);
-		Employee& getEmployee(const std::string& firstName,
-							  const std::string& lastName);
+    class Database {
+    public:
+        Employee& addEmployee(const std::string& firstName,
+                              const std::string& middleName,
+                              const std::string& lastName,
+                              const std::string& address);
+        Employee& getEmployee(int employeeNumber);
+        Employee& getEmployee(const std::string& firstName,
+                              const std::string& lastName);
+        void displayAll() const;
+        void fireEmployee(int employeeNumber);
+        void displayCurrent() const;
+        void displayFormer() const;
+        void save(std::ofstream& outputFile);
+        void loadFromFile(const std::string& filename);
+        void editEmployee(int employeeNumber, const std::string& address, int salary, bool hired);
 
-		void displayAll() const;
-		void fireEmployee(int employeeNumber);
-		void displayCurrent() const;
-		void displayFormer() const;
-		void save(std::ofstream& outputFile); 
-		void loadFromFile(const std::string& filename);// New function declaration for loading database from file
-
-
-	private:
-		std::vector<Employee> mEmployees;
-		int mNextEmployeeNumber = kFirstEmployeeNumber;
-	};
+    private:
+        std::vector<Employee> mEmployees;
+        int mNextEmployeeNumber = kFirstEmployeeNumber;
+    };
 }
